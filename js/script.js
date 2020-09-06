1f206e9cb567c750227bc77849a080d6a212ebba
Chermaley <slimix23@gmail.com>
2020-09-06 14:45:39 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+let number = 4; 
+console.log('string'*9);
+
+const person = "Alex";
+const bool = false;
+
+const obj = {
+    name: "John",
+    age: 25
+};
+console.log(obj.age);
+//alert('Hello')
+//const result = confirm("Are you here?");
+//console.log(result);
+//const answer = +prompt("Вам есть 18?", "");
+//console.log(answer);
+
+const answer = [];
+
+answer[0] = prompt("How are you?", "" );
+answer[1] = prompt("How ?", "" );
+answer[2] = prompt("are you?", "" );
+document.write(answer);
